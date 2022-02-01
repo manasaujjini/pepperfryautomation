a46abe0338c74d50d38fdb9e7d5bd5db9d253589
manasaujjini <mansiujjini@gmail.com>
2022-02-01 14:48:18 +0530

    added lowest price scenario

--- cypress/integration/RadioButton/LowestPrice.js
+++ cypress/integration/RadioButton/LowestPrice.js
@@ -1,21 +1,21 @@
 /// <reference types="cypress" />
 
 describe("My first project", function () {
-    let highestPriceArray = [];
+    let lowestPriceArray = [];
     it("My project", function () {
 
         cy.visit('https://www.pepperfry.com/')
         cy.get('#search').should('be.visible').type('study table')
         cy.get('#searchButton').click()
-        cy.get('label[for="price-desc"]').click()
+        cy.get('label[for="price-asc"]').click()
         cy.wait(5000);
         //cy.intercept("GET", "/site_product/search?q=study%20table&as=0&src=study%20table&forder=&order=price&dir=desc&p=1").as("highestPrice")
         // cy.wait("@highestPrice", { timeout: 45000 });
         cy.get('.clipCard__price-offer').each($elem => {
-            highestPriceArray.push($elem.text())
+            lowestPriceArray.push($elem.text())
             cy.log("elemPrice", $elem.text())
         })
-        cy.log("highestPriceArray", highestPriceArray)
-        cy.get('.clipCard__price-offer').first().invoke('text').should("be.eq", highestPriceArray[1])
+        cy.log("lowestPriceArray", lowestPriceArray)
+        cy.get('.clipCard__price-offer').first().invoke('text').should("be.eq", lowestPriceArray[1])
     })
 })
